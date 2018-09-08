79b449bb77a9b8ab5ad4497518226fb6f664e55c
Radwa Ibrahim <radwa.m.ebrahim@gmail.com>
2018-09-08 17:28:40 +0200

    initial project setup

--- Gificient/GifListViewController.swift
+++ Gificient/GifListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GifListViewController.swift
 //  Gificient
 //
 //  Created by Radwa Muhammad on 08.09.18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GifListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
